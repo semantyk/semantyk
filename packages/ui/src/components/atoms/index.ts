0d9db1d4c33ccebe97e2b1040b728d0dee0ec900
Daniel B. <daniel@semantyk.com>
2025-03-19 12:45:05 -0600

    🧹 Chore: Add index for ui atom components

---
+++ packages/ui/src/components/atoms/index.ts
@@ -0,0 +1,21 @@
+/**
+ * –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+ * # `index.ts`
+ * @organization: Semantyk
+ * @project: UI
+ *
+ * @file: This file contains the index for the atom components.
+ *
+ * @created: Mar 19, 2025
+ * @modified: Mar 19, 2025
+ *
+ * @author: Semantyk Team
+ * @maintainer: Daniel Bakas <https://id.danielbakas.com>
+ *
+ * @copyright: Semantyk © 2025. All rights reserved.
+ * –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+ */
+
+//* Exports
+export * from './Body';
+export * from './HTML';
