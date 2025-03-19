fb024aa85293aea02d0d7ada2f4d66e4d5715acb
Daniel B. <daniel@semantyk.com>
2025-03-19 12:44:12 -0600

    🧹 Chore: Add index for ui package

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,1 +1,20 @@
-// Test
+/**
+ * –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+ * # `index.ts`
+ * @organization: Semantyk
+ * @project: UI
+ *
+ * @file: This file contains the index for the UI package.
+ *
+ * @created: Mar 19, 2025
+ * @modified: Mar 19, 2025
+ *
+ * @author: Semantyk Team
+ * @maintainer: Daniel Bakas <https://id.danielbakas.com>
+ *
+ * @copyright: Semantyk © 2025. All rights reserved.
+ * –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+ */
+
+//* Exports
+export * from './components';
