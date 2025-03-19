4ffc24eae92191cb5faf40803336c8337d96b246
Daniel B. <daniel@semantyk.com>
2025-03-19 12:50:33 -0600

    ✨ Feature: Add Body atom component to ui

---
+++ packages/ui/src/components/atoms/Body/Body.types.ts
@@ -0,0 +1,29 @@
+/**
+ * –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+ * # `Body.types.ts`
+ * @organization: Semantyk
+ * @project: UI
+ *
+ * @file: This file contains the types for the Body component.
+ *
+ * @created: Mar 19, 2025
+ * @modified: Mar 19, 2025
+ *
+ * @author: Semantyk Team
+ * @maintainer: Daniel Bakas <https://id.danielbakas.com>
+ *
+ * @copyright: Semantyk © 2025. All rights reserved.
+ * –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+ */
+
+//* Imports
+import { ReactNode } from 'react';
+import type { HTMLAttributes } from 'react';
+
+//* Main
+type BodyProps = HTMLAttributes<HTMLBodyElement> & {
+    children?: ReactNode;
+};
+
+//* Exports
+export type { BodyProps };
