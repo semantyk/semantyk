dae73f7be171b86de3f0972c1972306f77a46927
Daniel B. <daniel@semantyk.com>
2025-03-19 12:49:12 -0600

    🧹 Chore: Add molecules module to ui

---
+++ packages/ui/src/components/molecules/index.ts
@@ -0,0 +1,19 @@
+/**
+ * –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+ * # `index.ts`
+ * @organization: Semantyk
+ * @project: UI
+ *
+ * @file: This file contains the index for the molecules components.
+ *
+ * @created: Mar 19, 2025
+ * @modified: Mar 19, 2025
+ *
+ * @author: Semantyk Team
+ * @maintainer: Daniel Bakas <https://id.danielbakas.com>
+ *
+ * @copyright: Semantyk © 2025. All rights reserved.
+ * –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+ */
+
+//* Exports
