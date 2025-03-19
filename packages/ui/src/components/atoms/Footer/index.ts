e09941fe7ad0bf06d22b3610f93fbf4d1d6e4ad7
Daniel B. <daniel@semantyk.com>
2025-03-19 13:21:02 -0600

    ✨ Feature: Add Footer atom component to ui

---
+++ packages/ui/src/components/atoms/Footer/index.ts
@@ -0,0 +1,21 @@
+/**
+ * –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+ * # `index.ts`
+ * @organization: Semantyk
+ * @project: UI
+ *
+ * @file: This file contains the index for the Footer component.
+ *
+ * @created: Mar 19, 2025
+ * @modified: Mar 19, 2025
+ *
+ * @author: Semantyk Team
+ * @maintainer: Daniel Bakas <https://id.danielbakas.com>
+ *
+ * @copyright: Semantyk © 2025. All rights reserved.
+ * –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+ */
+
+//* Exports
+export * from './Footer';
+export * from './Footer.types';
